56f64974bad2fe2ef24348401bf7b058ee54ca6e
João Guilherme <joaoguilherme2013@outlook.com.br>
2025-12-23 12:21:07 -0300

    coomit

---
+++ Estruturas condiconais e de repetição/Estruturas condicionais.py
@@ -0,0 +1,26 @@
+saldo = 2000.0
+saque = float(input("Informe o valor do saque:"))
+
+if saldo >= saque:
+  print("Realizando saque!")
+
+if saldo < saque:
+  print("saldo insuficiente!")
+
+
+MAIOR_IDADE = 18
+
+idade = int(input("informe sua idade: "))
+
+if idade >= MAIOR_IDADE:
+   print("Maior de idade, pode tirar a CNH.")
+
+if idade < MAIOR_IDADE:
+  print("Ainda não pode tirar a CNH.")
+
+if idade >= MAIOR_IDADE:
+  print("Maior de idade, pode tirar a CNH.")
+else: idade <= MAIOR_IDADE
+print("Ainda não pode tirar a CNH.")
+
+32
